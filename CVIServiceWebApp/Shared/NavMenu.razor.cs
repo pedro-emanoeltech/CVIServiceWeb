3e66b45efd3b2dac4fd5f199596f5fcbe9569e83
Pedro Emanoel <pedro.emanoeltech@hotmail.com>
2022-11-19 19:20:36 -0300

    Add nav

--- CVIServiceWebApp/Shared/NavMenu.razor.cs
+++ CVIServiceWebApp/Shared/NavMenu.razor.cs
@@ -1,6 +1,11 @@
-﻿namespace CVIServiceWebApp.Shared
+﻿using static System.Net.WebRequestMethods;
+
+namespace CVIServiceWebApp.Shared
 {
     public partial class NavMenu 
     {
+        public string ImagePerfil { get; set; } = "https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png";
+
+        public string? Search { get; set; }
     }
 }
